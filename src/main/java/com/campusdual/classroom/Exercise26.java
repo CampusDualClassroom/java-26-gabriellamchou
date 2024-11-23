f9c7edb45cd18b8eeedcd319dec111ec222557b7
Gabriel <gabriel.lamela.ch@gmail.com>
2024-11-23 23:19:04 +0100

    Ejercicio 26 resuelto

--- src/main/java/com/campusdual/classroom/Exercise26.java
+++ src/main/java/com/campusdual/classroom/Exercise26.java
@@ -4,5 +4,22 @@ package com.campusdual.classroom;
 public class Exercise26 {
     public static void main(String[] args) {
 
+        Contact c1 = new Contact("Javier", "López", "111");
+        Contact c2 = new Contact("Carlos", "Fernández-Simón", "222");
+        Contact c3 = new Contact("Jose Manuel", "Soria", "333");
+        Contact c4 = new Contact("Santiago", "Fernández Rocha", "444");
+        Contact c5 = new Contact("Esteban", "Serrano del Río", "555");
+        Contact c6 = new Contact("Fernando Miguel", "Juan de los Santos Requejo León", "666");
+
+        Phonebook listin = new Phonebook();
+
+        listin.addContact(c1);
+        listin.addContact(c2);
+        listin.addContact(c3);
+        listin.addContact(c4);
+        listin.addContact(c5);
+        listin.addContact(c6);
+
+        listin.phonebookMenu();
     }
 }
--- src/main/java/com/campusdual/classroom/Phonebook.java
+++ src/main/java/com/campusdual/classroom/Phonebook.java
@@ -1,5 +1,79 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Phonebook {
 
+    private Map<String, Contact> contactList;
+
+    public Phonebook() {
+        this.contactList = new HashMap<>(); // Inicializa el mapa
+    }
+
+    public void addContact(Contact contact) {
+        this.contactList.put(contact.getCode(), contact);
+    }
+
+    public void deleteContact(String code) {
+        this.contactList.remove(code);
+    }
+
+    public Map<String, Contact> getData() {
+        return this.contactList;
+    }
+
+    public void showPhonebook() {
+        for (Contact contacto : this.contactList.values()) {
+            System.out.println("Nombre: " + contacto.getName() +
+                    ". Apellidos: " + contacto.getSurnames() +
+                    ". Teléfono: " + contacto.getPhone() +
+                    ". Código: " + contacto.getCode() + ".");
+        }
+    }
+
+    public void phonebookMenu() {
+
+        int userOption = 0;
+
+        while (userOption != 5) {
+            userOption = Utils.integer("Seleccione una acción: \n" +
+                    "1- Añadir un contacto\n" +
+                    "2- Mostrar contactos\n" +
+                    "3- Seleccionar contacto\n" +
+                    "4- Eliminar un contacto\n" +
+                    "5- Salir\n");
+
+            switch (userOption) {
+                case 1:
+                    String name = Utils.string("Nombre: ");
+                    String surnames = Utils.string("Apellidos: ");
+                    String phone = Utils.string("Teléfono: ");
+                    Contact contact = new Contact(name, surnames, phone);
+                    addContact(contact);
+                    System.out.println("Contacto añadido!");
+                    break;
+                case 2:
+                    showPhonebook();
+                    break;
+                case 3:
+                    String contactToSelect = Utils.string("Introduzca el código: ");
+                    this.contactList.get(contactToSelect).contactMenu();
+                    break;
+                case 4:
+                    String contactToDelete = Utils.string("Introduzca el código: ");
+                    this.contactList.remove(contactToDelete);
+                    System.out.println("Contacto eliminado!");
+                    break;
+                case 5:
+                    break;
+            }
+        }
+    }
+
+    public Map<String, Contact> getContactList() {
+        return contactList;
+    }
 }
